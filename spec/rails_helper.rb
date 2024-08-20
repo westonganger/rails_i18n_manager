029994b5dc6b3f7aa4ebd368925cf0279931bec5
Weston Ganger <weston@westonganger.com>
2024-08-20 09:32:20 -0700

    Add Rails 7.2 to CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,8 +16,6 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
