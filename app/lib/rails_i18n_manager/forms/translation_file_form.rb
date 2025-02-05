37c747b190b5d5cec625b8465aa1e3a18908d62e
Weston Ganger <weston@westonganger.com>
2025-02-04 23:57:07 -0800

    Only call File.read once for import (#27)

--- app/lib/rails_i18n_manager/forms/translation_file_form.rb
+++ app/lib/rails_i18n_manager/forms/translation_file_form.rb
@@ -17,36 +17,53 @@ module RailsI18nManager
         @mark_inactive_translations = ["1", "true", "t"].include?(val.to_s.downcase)
       end
 
+      def file_extname
+        @file_extname ||= File.extname(file)
+      end
+
+      def file_contents_string
+        @file_contents_string ||= file.read
+      end
+
+      def parsed_file_contents
+        if defined?(@parsed_file_contents)
+          return @parsed_file_contents
+        end
+
+        case file_extname
+        when ".yml", ".yaml"
+          @parsed_file_contents = YAML.safe_load(file_contents_string)
+        when ".json"
+          @parsed_file_contents = JSON.parse(file_contents_string)
+        end
+      end
+
       def validate_file
         if file.blank?
           errors.add(:file, "Must upload a valid translation file.")
           return
         end
 
-        file_extname = File.extname(file)
-
         if [".yml", ".yaml", ".json"].exclude?(file_extname)
           errors.add(:file, "Invalid file format. Must be yaml or json file.")
           return
         end
 
-        file_contents = File.read(file)
-
-        if file_contents.blank?
+        if file_contents_string.blank?
           errors.add(:file, "Empty file provided.")
           return
         end
 
         case file_extname
         when ".yml", ".yaml"
-          if !YAML.safe_load(file_contents).is_a?(Hash)
+          if !parsed_file_contents.is_a?(Hash)
             errors.add(:file, "Invalid #{file_extname.sub(".","")} file.")
             return
           end
 
         when ".json"
           begin
-            JSON.parse(file_contents)
+            parsed_file_contents
           rescue JSON::ParserError
             errors.add(:file, "Invalid json file.")
             return
